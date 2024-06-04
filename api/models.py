c10aec8ecb69cef749602527dd338177799bc181
Mykola Setrakov <nikset1@gmail.com>
2024-06-04 15:00:04 +0300

    create Crew model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ("last_name",)
