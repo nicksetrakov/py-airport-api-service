868dd1a42e58a1a521b1ce167fcf3f1a7e7220de
Mykola Setrakov <nikset1@gmail.com>
2024-06-04 18:11:41 +0300

    create AirplaneTypeViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
-from airport.models import Crew
-from airport.serializers import CrewSerializer
+from airport.models import Crew, AirplaneType
+from airport.serializers import CrewSerializer, AirplaneTypeSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
     permission_classes = (IsAdminUser,)
