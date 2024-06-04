f20c875ecc2703467b1c9db9b68ea170cfa06a23
Mykola Setrakov <nikset1@gmail.com>
2024-06-04 17:27:00 +0300

    create CrewViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from airport.models import Crew
+from airport.serializers import CrewSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = (IsAdminUser,)
