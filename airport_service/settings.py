1c888c1f35de05da4cf79ba7f5818eb806d4faac
Mykola Setrakov <nikset1@gmail.com>
2024-06-04 18:09:03 +0300

    add token_blacklist to INSTALLED_APPS

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "drf_spectacular",
     "airport",
     "user"
