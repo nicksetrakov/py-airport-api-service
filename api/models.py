2e8908b977163d162f38b7466efccabc105965d8
Mykola Setrakov <nikset1@gmail.com>
2024-06-04 15:12:30 +0300

    create Airplane and AirplaneType models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,8 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
 
 
 class Crew(models.Model):
@@ -10,3 +14,37 @@ class Crew(models.Model):
 
     class Meta:
         ordering = ("last_name",)
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+def airplane_image_file_path(instance: "Airplane", filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+    image = models.ImageField(null=True, upload_to=airplane_image_file_path)
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("airplane_type", "-capacity")
