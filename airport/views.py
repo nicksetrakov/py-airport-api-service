8c98de0221315b2f141fd06ca218df516f91eceb
Mykola Setrakov <nikset1@gmail.com>
2024-06-05 17:57:07 +0300

    create CityViewSet

--- airport/views.py
+++ airport/views.py
@@ -4,9 +4,10 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
-from airport.models import Crew, AirplaneType, Airplane, Country
+from airport.models import Crew, AirplaneType, Airplane, Country, City
 from airport.serializers import CrewSerializer, AirplaneTypeSerializer, AirplaneSerializer, AirplaneListSerializer, \
-    AirplaneDetailSerializer, AirplaneImageSerializer, CountrySerializer
+    AirplaneDetailSerializer, AirplaneImageSerializer, CountrySerializer, CitySerializer, CityListSerializer, \
+    CityDetailSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -62,3 +63,18 @@ class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = (IsAdminUser,)
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.select_related("country")
+    serializer_class = CitySerializer
+    permission_classes = (IsAdminUser,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CityListSerializer
+
+        if self.action == "retrieve":
+            return CityDetailSerializer
+
+        return self.serializer_class
