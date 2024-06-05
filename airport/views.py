ad5322ae980d0695f1374de14b67012a041cd112
Mykola Setrakov <nikset1@gmail.com>
2024-06-06 00:11:11 +0300

    create AirportViewSet

--- airport/views.py
+++ airport/views.py
@@ -4,10 +4,22 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
-from airport.models import Crew, AirplaneType, Airplane, Country, City
-from airport.serializers import CrewSerializer, AirplaneTypeSerializer, AirplaneSerializer, AirplaneListSerializer, \
-    AirplaneDetailSerializer, AirplaneImageSerializer, CountrySerializer, CitySerializer, CityListSerializer, \
-    CityDetailSerializer
+from airport.models import Crew, AirplaneType, Airplane, Country, City, Airport
+from airport.serializers import (
+    CrewSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirplaneListSerializer,
+    AirplaneDetailSerializer,
+    AirplaneImageSerializer,
+    CountrySerializer,
+    CitySerializer,
+    CityListSerializer,
+    CityDetailSerializer,
+    AirportSerializer,
+    AirportDetailSerializer,
+    AirportListSerializer,
+)
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -25,13 +37,23 @@ class AirplaneTypeViewSet(viewsets.ModelViewSet):
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.select_related("airplane_type")
     serializer_class = AirplaneSerializer
-
+    permission_classes = (IsAdminUser,)
 
     def get_permissions(self):
         if self.action in ("list", "retrieve"):
-            return [IsAuthenticated(),]
-        if self.action in ("create", "update", "partial_update", "upload_image", "destroy"):
-            return [IsAdminUser(),]
+            return [
+                IsAuthenticated(),
+            ]
+        if self.action in (
+            "create",
+            "update",
+            "partial_update",
+            "upload_image",
+            "destroy",
+        ):
+            return [
+                IsAdminUser(),
+            ]
 
         return super().get_permissions()
 
@@ -47,7 +69,11 @@ class AirplaneViewSet(viewsets.ModelViewSet):
 
         return self.serializer_class
 
-    @action(methods=["POST"], detail=True, url_path="upload-image", )
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
     def upload_image(self, request, pk=None):
         item = self.get_object()
         serializer = self.get_serializer(item, data=request.data)
@@ -78,3 +104,27 @@ class CityViewSet(viewsets.ModelViewSet):
             return CityDetailSerializer
 
         return self.serializer_class
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.select_related(
+        "closest_big_city__country",
+    )
+    serializer_class = AirportSerializer
+
+    def get_permissions(self):
+
+        if self.action in ("list", "retrieve"):
+            return [
+                IsAuthenticated(),
+            ]
+
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return AirportDetailSerializer
+        if self.action == "list":
+            return AirportListSerializer
+
+        return super().get_serializer_class()
