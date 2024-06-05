25913e971c575531e880e421c281939e5278e385
Mykola Setrakov <nikset1@gmail.com>
2024-06-05 17:56:41 +0300

    register router cities

--- airport/urls.py
+++ airport/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from airport.views import (
-    CrewViewSet, AirplaneTypeViewSet, AirplaneViewSet, CountryViewSet,
+    CrewViewSet, AirplaneTypeViewSet, AirplaneViewSet, CountryViewSet, CityViewSet,
 )
 
 router = routers.DefaultRouter()
@@ -10,6 +10,7 @@ router.register("crews", CrewViewSet)
 router.register("airplane-types", AirplaneTypeViewSet)
 router.register("airplanes", AirplaneViewSet)
 router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
