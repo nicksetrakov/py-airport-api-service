f850a629a6d6c5c96fd677d228ac5d2d0688323e
Mykola Setrakov <nikset1@gmail.com>
2024-06-10 18:27:55 +0300

    fix checking tables

--- airport/management/commands/check_db_empty.py
+++ airport/management/commands/check_db_empty.py
@@ -2,6 +2,7 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
+from django.apps import apps
 
 
 class Command(BaseCommand):
@@ -12,14 +13,24 @@ class Command(BaseCommand):
         try:
             connection = connections["default"]
             cursor = connection.cursor()
-            cursor.execute(
-                "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';"
+            # Get a list of models from the "airport" app
+            airport_app_config = apps.get_app_config("airport")
+            models = airport_app_config.get_models()
+            # Check if there are any rows in any of the tables for the models
+            is_empty = all(
+                self.is_model_table_empty(cursor, model) for model in models
             )
-            row = cursor.fetchone()
-            if row[0] == 0:
+
+            if is_empty:
                 self.stdout.write("Database is empty, generating fake data...")
                 call_command("generate_fake_data")
             else:
                 self.stdout.write("Database is not empty, no action taken.")
         except OperationalError:
             self.stdout.write("Database is not available.")
+
+    def is_model_table_empty(self, cursor, model):
+        table_name = model._meta.db_table
+        cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
+        row = cursor.fetchone()
+        return row[0] == 0
