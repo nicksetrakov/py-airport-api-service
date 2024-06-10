9e32366691602946f31f7fdfc5bbf32337488ec0
Mykola Setrakov <nikset1@gmail.com>
2024-06-10 23:54:11 +0300

    add cash to FlightViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,4 +1,6 @@
 from django.db.models import Count, F, ExpressionWrapper, IntegerField
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema_view
 from rest_framework import viewsets, status
@@ -290,6 +292,14 @@ class FlightViewSet(viewsets.ModelViewSet):
             return FlightDetailSerializer
 
         return super().get_serializer_class()
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 @extend_schema_view(
