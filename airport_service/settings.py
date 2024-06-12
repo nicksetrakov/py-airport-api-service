e68d4ad7d14973e287717e74e3533bf036d8198d
Mykola Setrakov <nikset1@gmail.com>
2024-06-12 11:46:32 +0300

    add getting SECRET_KEY from environment

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-4k$nlrnk04n0ug!)_kd5l(f(fyph1z!^5h77z2lx-++7@(+xwg"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
